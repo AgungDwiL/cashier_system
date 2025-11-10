f21ef816784421954f1ae4e5c030e78e76dbb4dd
Agung Dwi Laksana <agung.laksana16@gmail.com>
2025-11-10 10:02:07 +0700

    membuat table role untuk menampilkan data role

--- views/roles.php
+++ views/roles.php
@@ -9,19 +9,34 @@
     $roles = $data->getAllRoles();
 ?>
 
-<table>
-    <thead>
-        <tr>
-            <td>No</td>
-            <td>Nama Role</td>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($roles as $no => $r) : ?>
-            <tr>
-                <td><?= $no+1 ?></td>
-                <td><?= $r['name'] ?></td>
-            </tr>
-        <?php endforeach; ?>
-    </tbody>
-</table>
+<!--begin::Row-->
+<div class="row">
+    <div class="col-md-12">
+        <div class="card mb-4">
+            <div class="card-header">
+                <h3 class="card-title">Data Roles</h3>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body p-0">
+            <table class="table">
+                <thead>
+                <tr>
+                    <th style="width: 10px">No</th>
+                    <th>Nama Role</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($roles as $no => $r) : ?>
+                        <tr class="align-middle">
+                            <td><?= $no+1 ?></td>
+                            <td><?= $r['name'] ?></td>
+                            
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+            </div>
+            <!-- /.card-body -->
+        </div>
+    </div>
+</div>
